256eb9345aa82ca8ec8983fdf449687190b5bd0d
pgarr <garlej.p@gmail.com>
2024-02-12 22:07:43 +0100

    pagination; static generation

--- src/api/products.ts
+++ src/api/products.ts
@@ -16,6 +16,23 @@ export const getProductsList = async (): Promise<Product[]> => {
 	return productsResponse.map(mapProductResponseToProduct);
 };
 
+export const getProductsListCount = async (): Promise<number> => {
+	const response = await fetch("https://naszsklep-api.vercel.app/api/products?take=9999999");
+	const productsResponse = JSON.parse(await response.text()) as ProductResponse[];
+	return productsResponse.length;
+};
+
+export const getProductsListPaginated = async (
+	page: number,
+	take: number = 20,
+): Promise<Product[]> => {
+	const response = await fetch(
+		`https://naszsklep-api.vercel.app/api/products?take=${take}&offset=${(page - 1) * take}`,
+	);
+	const productsResponse = JSON.parse(await response.text()) as ProductResponse[];
+	return productsResponse.map(mapProductResponseToProduct);
+};
+
 export const getProduct = async (id: string) => {
 	const response = await fetch(`https://naszsklep-api.vercel.app/api/products/${id}`);
 	const productResponse = JSON.parse(await response.text()) as ProductResponse;
