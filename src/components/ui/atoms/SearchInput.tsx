653854b885b5c3395a01b384db688f314492e38f
pgarr <garlej.p@gmail.com>
2024-02-24 22:29:06 +0100

    debounce search

--- src/components/ui/atoms/SearchInput.tsx
+++ src/components/ui/atoms/SearchInput.tsx
@@ -1,14 +1,21 @@
 "use client";
 
+import { useRef, useState } from "react";
 import { useRouter } from "next/navigation";
 
 export const SearchInput = () => {
+	const [search, setSearch] = useState("");
+	const timeoutRef = useRef<NodeJS.Timeout | null>(null);
 	const router = useRouter();
+
+	const redirectToSearch = () => {
+		router.push(`/search?query=${search}`);
+	};
 
 	return (
 		<form
-			action={(formData) => {
-				router.push(`/search?query=${String(formData.get("search"))}`);
+			action={() => {
+				redirectToSearch();
 			}}
 		>
 			<input
@@ -16,6 +23,16 @@ export const SearchInput = () => {
 				className="rounded-md border-2 border-gray-300 p-2"
 				type="search"
 				placeholder="Search"
+				value={search}
+				onChange={(e) => {
+					setSearch(e.target.value);
+					if (timeoutRef.current) {
+						clearTimeout(timeoutRef.current);
+					}
+					if (e.target.value.length > 2) {
+						timeoutRef.current = setTimeout(redirectToSearch, 500);
+					}
+				}}
 			/>
 		</form>
 	);
