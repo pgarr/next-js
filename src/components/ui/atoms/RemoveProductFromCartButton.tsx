82796a97e0de3a445a83000205574eb97c9451df
pgarr <garlej.p@gmail.com>
2024-02-27 09:36:24 +0100

    transition for remove

--- src/components/ui/atoms/RemoveProductFromCartButton.tsx
+++ src/components/ui/atoms/RemoveProductFromCartButton.tsx
@@ -1,3 +1,7 @@
+"use client";
+
+import { useTransition } from "react";
+import { useRouter } from "next/navigation";
 import { removeProductFromCartAction } from "@/api/actions";
 
 export const RemoveProductFromCartButton = ({
@@ -7,12 +11,18 @@ export const RemoveProductFromCartButton = ({
 	cartId: string;
 	productId: string;
 }) => {
+	const router = useRouter();
+	const [isPending, startTransition] = useTransition();
+
 	return (
 		<button
+			disabled={isPending}
 			type="submit"
-			formAction={async () => {
-				"use server";
-				await removeProductFromCartAction(cartId, productId);
+			onClick={() => {
+				startTransition(async () => {
+					await removeProductFromCartAction(cartId, productId);
+					router.refresh();
+				});
 			}}
 			className="ml-4 text-sm font-medium text-indigo-600 hover:text-indigo-500 sm:ml-0 sm:mt-3"
 		>
